959ce148bc0a7b8558fd476bc204e1cf4b45b67b
SobakaBekka <st69704@upce.cz>
2024-02-25 14:29:21 +0100

    done first 3 tasks

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
+
+        int summa = 0;
+        for (int count = firstBoarder; firstBoarder <= secondBoarder; firstBoarder++) {
+            summa += count;
+        }
+        System.out.println(summa);
+
     }
 }
+//Диапазон сумм.
+//Напишите программу, которая выводит сумму между числами, заданными в качестве аргументов метода:
