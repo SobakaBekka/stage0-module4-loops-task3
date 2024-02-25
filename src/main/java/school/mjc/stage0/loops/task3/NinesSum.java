ff1fdb5d3967bee9dc8183c084cc816635ad44f4
SobakaBekka <st69704@upce.cz>
2024-02-25 23:46:18 +0100

    fixed the bugs

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,14 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
         int summa = 0;
         int number = 0;
-        for (int count = 0; count < lengthOfLastNumber; count++){
+        for (int count = 0; count < lengthOfLastNumber; count++) { // Изменено условие на count < lengthOfLastNumber
             number = number * 10 + 9;
             summa += number;
         }
         System.out.println(summa);
-
     }
 }
