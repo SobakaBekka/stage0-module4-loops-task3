9ff8c933896cdf966ffebe5e7db6a64d6f17a2bc
SobakaBekka <st69704@upce.cz>
2024-02-26 00:09:32 +0100

    fixed the bugs

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,13 +1,15 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public void printDigitsSum(int t) {
+
         String numberAsString = Math.abs(t) + "";
         int summa = 0;
 
         for (int count = 0; count < numberAsString.length(); count++) {
             summa += Character.getNumericValue(numberAsString.charAt(count));
         }
+
         System.out.println(summa);
     }
 }
